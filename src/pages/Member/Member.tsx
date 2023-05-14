e5bb8782b56a487f916dfc3e3a821f7a02970a8e
UBiiiii <willowman55@gmail.com>
2023-05-15 00:41:21 +0900

    redux test page

--- src/pages/Member/Member.tsx
+++ src/pages/Member/Member.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Login from "../Login";
+import axios from "axios";
+import { login } from "../../redux/slice/userSilce";
+import { AppDispatch } from "../../redux/store";
+
 const Member = () => {
+	const user = useSelector((state:any)=>(state.user.data));
+	const dispatch = useDispatch<AppDispatch>();
+
+	const doLogin = async () => {
+		await dispatch(login());
+	}
+
+	useEffect(()=>{
+
+	},[])
+
 	return(
 		<div>
 			MemberPage
