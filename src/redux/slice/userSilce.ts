88bc151de95e4ba2bbdcb080cded53d412caa278
UBiiiii <willowman55@gmail.com>
2023-05-15 00:41:05 +0900

    add package redux and proxy

---
+++ src/redux/slice/userSilce.ts
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const login = createAsyncThunk("loginState", async (_:void, {rejectWithValue})=>{
+    try {
+        const userData = (await axios.get("/api/login",{data:{userId:"abcd", password:"defg"}})).data;
+        return userData;
+    } catch (error) {
+    }
+})
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState:{
+        data: {},
+        loading: false
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.data = action.payload;
+        }
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(login.pending,(state, action)=>{
+            state.loading = true;
+        })
+        builder.addCase(login.fulfilled,(state, action)=>{
+            state.data = action.payload;
+            state.loading = false;
+        });
+    }
+})
+
+export const { setUser } = userSlice.actions
+export default userSlice.reducer
