526618cca429194c75c0f556f9752177dd269f6e
gitsegun <gitsegun44@gmail.com>
2023-05-16 20:11:56 +0900

    [feature] #11 PartTime  사용자 정보 관련 페이지 기본 컴포넌트화

--- src/pages/PartTime/Info.tsx
+++ src/pages/PartTime/Info.tsx
@@ -1,3 +1,6 @@
+import IndexHeader from "../../components/Header/IndexHeader";
+import NavBar from "../../components/NavBar/NavBar";
+
 const Info = () => {
     return (
         <div className="w-[375px] h-[812px] relative overflow-hidden bg-white">
@@ -165,59 +168,8 @@ const Info = () => {
                     fill="white"
                 />
             </svg>
-            <div className="w-[375px] h-[53px] absolute left-0 top-[759px] overflow-hidden bg-[#454545]">
-                <div className="w-[125px] h-[53px] absolute left-[249px] top-[-1px] bg-[#454545]" />
-                <div className="w-[125px] h-[53px] absolute left-[124px] top-[-1px] rounded-tr-[20px] bg-[#d9d9d9]" />
-                <div className="w-[125px] h-[53px] absolute left-[-1px] top-[-1px] bg-[#d9d9d9]" />
-                <div className="w-6 h-6 absolute left-[301px] top-3" />
-                <svg
-                    width={24}
-                    height={27}
-                    viewBox="0 0 24 27"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-[25.96px] absolute left-[301px] top-[13.25px]"
-                    preserveAspectRatio="none"
-                >
-                    <path
-                        d="M2.66663 23.3248H21.3333V20.4405H2.66663V23.3248ZM2.66663 17.5561H21.3333V14.6718H2.66663V17.5561ZM2.66663 3.13437V6.01872H21.3333V3.13437H2.66663ZM2.66663 11.7874H21.3333V8.90308H2.66663V11.7874Z"
-                        fill="white"
-                    />
-                </svg>
-                <div className="w-[23px] h-[39.26px]">
-                    <div className="absolute left-[198.5px] top-[5.5px]" />
-                    <img
-                        className="w-[23.82px] h-[39px] absolute left-[198.5px] top-[5.5px]"
-                        src="small_logo.png"
-                    />
-                </div>
-            </div>
-            <div className="w-[331px] h-[125px] absolute left-[22px] top-0 overflow-hidden">
-                <div className="w-[331px] h-[30px] absolute left-0 top-0 overflow-hidden" />
-                <div className="w-[331px] h-[95px] absolute left-0 top-[30px] overflow-hidden">
-                    <svg
-                        width={74}
-                        height={95}
-                        viewBox="0 0 74 95"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-[74px] h-[95px] absolute left-[257px] top-0"
-                        preserveAspectRatio="none"
-                    >
-                        <circle cx={37} cy={48} r={3} fill="#FF9F0E" />
-                        <circle cx={28} cy={48} r={3} fill="#FF9F0E" />
-                        <circle cx={46} cy={48} r={3} fill="#FF9F0E" />
-                    </svg>
-                    <div className="w-[257px] h-[95px] absolute left-0 top-0 overflow-hidden">
-                        <div className="w-10 h-[95px] absolute left-0 top-0 overflow-hidden" />
-                        <div className="w-[217px] h-[95px] absolute left-[50px] top-0 overflow-hidden">
-                            <p className="w-[217px] h-[95px] absolute left-0 top-0 text-[40px] font-medium text-left text-white">
-                                김고객
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <NavBar state="3" />
+            <IndexHeader title="김고객" />
         </div>
     )
 }
