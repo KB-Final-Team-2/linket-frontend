2f978ca246c5fe9d590f655557762baf1054a3d0
skybluelion <brotherjun96@gmail.com>
2023-05-16 20:10:32 +0900

    [Feat] #10 Staff 공지사항 및 FAQ 관련 페이지 컴포넌트화

--- src/pages/Staff/InfoNotice.tsx
+++ src/pages/Staff/InfoNotice.tsx
@@ -1,4 +1,9 @@
+import Header from "../../components/Header/Header";
+import NextList from "../../components/List/NextList";
+
 const InfoNotice = () => {
+    const list = [{ title: "안녕하세요. 링켓 공지사항입니다.", content: "반갑습니다. 링켓 공지사항 내용입니다." },{ title: "안녕하세요. 링켓 공지사항입니다.", content: "반갑습니다. 링켓 공지사항 내용입니다." },{ title: "안녕하세요. 링켓 공지사항입니다.", content: "반갑습니다. 링켓 공지사항 내용입니다." },{ title: "안녕하세요. 링켓 공지사항입니다.", content: "반갑습니다. 링켓 공지사항 내용입니다." },{ title: "안녕하세요. 링켓 공지사항입니다.", content: "반갑습니다. 링켓 공지사항 내용입니다." }];
+
     return (
         <div className="w-[375px] h-[812px] relative overflow-hidden bg-white">
             <svg
@@ -12,160 +17,12 @@ const InfoNotice = () => {
             >
                 <path d="M0 0H375V812H0V0Z" fill="#454545" />
             </svg>
+            <Header title="공지사항"/>
             <div className="w-[331px] h-[622px] absolute left-[22px] top-[125px]" />
             <div className="w-[331px] h-[580px] absolute left-[22px] top-[125px] overflow-hidden border border-white">
-                <div className="w-[331px] h-[37px] absolute left-[-1px] top-[74px] overflow-hidden">
-                    <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                        <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                            <p className="w-[331px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-left text-white">
-                                안녕하세요. 링켓 공지사항입니다.
-                            </p>
-                        </div>
-                        <svg
-                            width={330}
-                            height={1}
-                            viewBox="0 0 330 1"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="absolute left-[-1px] top-9"
-                            preserveAspectRatio="none"
-                        >
-                            <line x1={-1} y1="0.5" x2={331} y2="0.5" stroke="white" />
-                        </svg>
-                    </div>
-                    <div className="flex justify-start items-start absolute left-[291px] top-[-0.5px] overflow-hidden gap-2.5 p-2">
-                        <svg
-                            width={16}
-                            height={20}
-                            viewBox="0 0 16 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="flex-grow-0 flex-shrink-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M1.25 2.63878L14 10L1.25 17.3612L1.25 2.63878Z"
-                                fill="#FF6000"
-                                stroke="#454545"
-                                strokeWidth={2}
-                            />
-                        </svg>
-                    </div>
-                </div>
-                <div className="w-[331px] h-[37px] absolute left-0 top-[111px] overflow-hidden">
-                    <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                        <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                            <p className="w-[331px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-left text-white">
-                                안녕하세요. 링켓 공지사항입니다.
-                            </p>
-                        </div>
-                        <svg
-                            width={331}
-                            height={1}
-                            viewBox="0 0 331 1"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="absolute left-[-1px] top-9"
-                            preserveAspectRatio="none"
-                        >
-                            <line y1="0.5" x2={332} y2="0.5" stroke="white" />
-                        </svg>
-                    </div>
-                    <div className="flex justify-start items-start absolute left-[291px] top-[-0.5px] overflow-hidden gap-2.5 p-2">
-                        <svg
-                            width={16}
-                            height={20}
-                            viewBox="0 0 16 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="flex-grow-0 flex-shrink-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M1.25 2.63878L14 10L1.25 17.3612L1.25 2.63878Z"
-                                fill="#FF6000"
-                                stroke="#454545"
-                                strokeWidth={2}
-                            />
-                        </svg>
-                    </div>
-                </div>
-                <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                    <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                        <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                            <p className="w-[331px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-left text-white">
-                                안녕하세요. 링켓 공지사항입니다.
-                            </p>
-                        </div>
-                        <svg
-                            width={331}
-                            height={1}
-                            viewBox="0 0 331 1"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="absolute left-[-1px] top-9"
-                            preserveAspectRatio="none"
-                        >
-                            <line y1="0.5" x2={332} y2="0.5" stroke="white" />
-                        </svg>
-                    </div>
-                    <div className="flex justify-start items-start absolute left-[291px] top-[-0.5px] overflow-hidden gap-2.5 p-2">
-                        <svg
-                            width={16}
-                            height={20}
-                            viewBox="0 0 16 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="flex-grow-0 flex-shrink-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M1.25 2.63878L14 10L1.25 17.3612L1.25 2.63878Z"
-                                fill="#FF6000"
-                                stroke="#454545"
-                                strokeWidth={2}
-                            />
-                        </svg>
-                    </div>
-                </div>
-                <div className="w-[331px] h-[37px] absolute left-[-1px] top-[37px] overflow-hidden">
-                    <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                        <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden">
-                            <p className="w-[331px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-left text-white">
-                                안녕하세요. 링켓 공지사항입니다.
-                            </p>
-                        </div>
-                        <svg
-                            width={330}
-                            height={1}
-                            viewBox="0 0 330 1"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="absolute left-[-1px] top-9"
-                            preserveAspectRatio="none"
-                        >
-                            <line x1={-1} y1="0.5" x2={331} y2="0.5" stroke="white" />
-                        </svg>
-                    </div>
-                    <div className="flex justify-start items-start absolute left-[291px] top-[-0.5px] overflow-hidden gap-2.5 p-2">
-                        <svg
-                            width={16}
-                            height={20}
-                            viewBox="0 0 16 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="flex-grow-0 flex-shrink-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M1.25 2.63878L14 10L1.25 17.3612L1.25 2.63878Z"
-                                fill="#FF6000"
-                                stroke="#454545"
-                                strokeWidth={2}
-                            />
-                        </svg>
-                    </div>
-                </div>
+                {list.map((el) => {
+                    return (<NextList title={el.content} />)
+                })}
             </div>
             <div className="w-[375px] h-[53px] absolute left-0 top-[759px] overflow-hidden bg-[#454545]">
                 <div className="w-[125px] h-[53px] absolute left-[249px] top-[-1px] bg-[#454545]" />
@@ -178,48 +35,6 @@ const InfoNotice = () => {
                         className="w-[23.82px] h-[39px] absolute left-[198.5px] top-[5.5px]"
                         src="small_logo.png"
                     />
-                </div>
-            </div>
-            <div className="w-[331px] h-[125px] absolute left-[22px] top-0">
-                <div className="w-[331px] h-[125px] absolute left-0 top-0 overflow-hidden">
-                    <div className="w-[331px] h-[30px] absolute left-0 top-0 overflow-hidden" />
-                    <div className="w-[331px] h-[95px] absolute left-0 top-[30px] overflow-hidden">
-                        <svg
-                            width={74}
-                            height={95}
-                            viewBox="0 0 74 95"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[74px] h-[95px] absolute left-[257px] top-0"
-                            preserveAspectRatio="none"
-                        >
-                            <circle cx={37} cy={48} r={3} fill="#FF6000" />
-                            <circle cx={28} cy={48} r={3} fill="#FF6000" />
-                            <circle cx={46} cy={48} r={3} fill="#FF6000" />
-                        </svg>
-                        <div className="w-[183px] h-[95px] absolute left-[74px] top-0 overflow-hidden">
-                            <p className="w-[183px] h-[95px] absolute left-0 top-0 text-[23px] text-center text-white">
-                                공지사항
-                            </p>
-                        </div>
-                        <svg
-                            width={74}
-                            height={95}
-                            viewBox="0 0 74 95"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[74px] h-[95px] absolute left-0 top-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M35 55L28 48M28 48L35 41M28 48L46 48"
-                                stroke="white"
-                                strokeWidth={2}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
-                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Staff/InfoNoticeContent.tsx
+++ src/pages/Staff/InfoNoticeContent.tsx
@@ -1,4 +1,7 @@
+import Header from "../../components/Header/Header";
+
 const InfoFAQContent = () => {
+    const content ={ title: "안녕하세요. 링켓 공지사항입니다.", content: "반갑습니다. 링켓 공지사항 내용입니다." }
     return (
         <div className="w-[375px] h-[812px] relative overflow-hidden bg-white">
             <svg
@@ -12,25 +15,24 @@ const InfoFAQContent = () => {
             >
                 <path d="M0 0H375V812H0V0Z" fill="#454545" />
             </svg>
+            <Header title="공지사항"/>
             <div className="w-[331px] h-[622px] absolute left-[22px] top-[125px]" />
             <div className="w-[331px] h-[580px] absolute left-[22px] top-[125px] overflow-hidden border border-white">
+
                 <div className="w-[331px] h-[490px] absolute left-0 top-[65px] overflow-hidden bg-[#c4c4c4]">
                     <p className="w-[331px] h-[535px] absolute left-0 top-0 text-[15px] font-bold text-left text-black">
                         <span className="w-[331px] h-[535px] text-[15px] font-bold text-left text-black">
-                            안녕하세요, 링켓입니다.
-                        </span>
-                        <br />
-                        <span className="w-[331px] h-[535px] text-[15px] font-bold text-left text-black">
-                            공지사항 테스트 중입니다.
+                            {content.content}
                         </span>
                     </p>
                 </div>
                 <div className="w-[331px] h-[45px] absolute left-0 top-0 overflow-hidden border-t-0 border-r-0 border-b border-l-0 border-white">
                     <p className="w-[331px] h-[45px] absolute left-0 top-0 text-[15px] font-bold text-left text-white">
                         {" "}
-                        안녕하세요. 링켓입니다.
+                        {content.title}
                     </p>
                 </div>
+                
             </div>
             <div className="w-[375px] h-[53px] absolute left-0 top-[759px] overflow-hidden bg-[#454545]">
                 <div className="w-[125px] h-[53px] absolute left-[249px] top-[-1px] bg-[#454545]" />
@@ -43,48 +45,6 @@ const InfoFAQContent = () => {
                         className="w-[23.82px] h-[39px] absolute left-[198.5px] top-[5.5px]"
                         src="small_logo.png"
                     />
-                </div>
-            </div>
-            <div className="w-[331px] h-[125px] absolute left-[22px] top-0">
-                <div className="w-[331px] h-[125px] absolute left-0 top-0 overflow-hidden">
-                    <div className="w-[331px] h-[30px] absolute left-0 top-0 overflow-hidden" />
-                    <div className="w-[331px] h-[95px] absolute left-0 top-[30px] overflow-hidden">
-                        <svg
-                            width={74}
-                            height={95}
-                            viewBox="0 0 74 95"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[74px] h-[95px] absolute left-[257px] top-0"
-                            preserveAspectRatio="none"
-                        >
-                            <circle cx={37} cy={48} r={3} fill="#FF6000" />
-                            <circle cx={28} cy={48} r={3} fill="#FF6000" />
-                            <circle cx={46} cy={48} r={3} fill="#FF6000" />
-                        </svg>
-                        <div className="w-[183px] h-[95px] absolute left-[74px] top-0 overflow-hidden">
-                            <p className="w-[183px] h-[95px] absolute left-0 top-0 text-[23px] text-center text-white">
-                                공지사항
-                            </p>
-                        </div>
-                        <svg
-                            width={74}
-                            height={95}
-                            viewBox="0 0 74 95"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[74px] h-[95px] absolute left-0 top-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M35 55L28 48M28 48L35 41M28 48L46 48"
-                                stroke="white"
-                                strokeWidth={2}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
-                    </div>
                 </div>
             </div>
         </div>
