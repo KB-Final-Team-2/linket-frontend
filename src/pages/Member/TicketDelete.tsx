0eadc6cd716283cbd2411487b34a174d9f01257b
UBiiiii <willowman55@gmail.com>
2023-05-16 17:04:48 +0900

    [Feat] #9 Ticket 기본 컴포넌트화

--- src/pages/Member/TicketDelete.tsx
+++ src/pages/Member/TicketDelete.tsx
@@ -1,6 +1,20 @@
+import Button from "../../components/Button/Button";
+import Header from "../../components/Header/Header";
+import List from "../../components/List/List";
+import NavBar from "../../components/NavBar/NavBar";
+
 const TicketDelete = () => {
+    const ticket = {
+        eventTitle : "KB It's your life",
+        eventType : "Concert",
+        startDate : "2023.03.02",
+        endDate: "2023.05.31",
+        place: "멀티캠퍼스 선릉",
+        url: "http://linket.io/event/12345",
+    }
+
     return (
-        <div className="w-[375px] h-[812px] relative overflow-hidden bg-white">
+        <div className="w-[375px] h-[812px] relative overflow-hidden bg-white flex justify-center">
             <svg
                 width={375}
                 height={812}
@@ -13,126 +27,29 @@ const TicketDelete = () => {
                 <path d="M0 0H375V812H0V0Z" fill="#454545" />
             </svg>
             <div className="w-[331px] h-[622px] absolute left-[22px] top-[125px]" />
-            <div className="w-[331px] h-[580px] absolute left-[22px] top-[125px] overflow-hidden border border-white">
-                <div className="w-[330px] h-[148px] absolute left-0 top-0 overflow-hidden border border-white">
-                    <div className="w-[331px] h-[37px] absolute left-0 top-[111px] overflow-hidden border-t-0 border-r-0 border-b border-l-0 border-white">
-                        <p className="w-[115.35px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-center text-white">
-                            행사 장소
-                        </p>
-                        <p className="w-[215.65px] h-[37px] absolute left-[115.35px] top-0 text-[15px] font-bold text-center text-white">
-                            멀티캠퍼스 선릉
-                        </p>
-                    </div>
-                    <div className="w-[331px] h-[37px] absolute left-0 top-[74px] overflow-hidden border-t-0 border-r-0 border-b border-l-0 border-white">
-                        <p className="w-[115.35px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-center text-white">
-                            행사 기간
-                        </p>
-                        <p className="w-[215.65px] h-[37px] absolute left-[115.35px] top-0 text-[15px] font-bold text-center text-white">
-                            2023.03.02 ~ 2023.05.31
-                        </p>
-                    </div>
-                    <div className="w-[331px] h-[37px] absolute left-0 top-[37px] overflow-hidden border-t-0 border-r-0 border-b border-l-0 border-white">
-                        <p className="w-[115.35px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-center text-white">
-                            행사 분류
-                        </p>
-                        <p className="w-[215.65px] h-[37px] absolute left-[115.35px] top-0 text-[15px] font-bold text-center text-white">
-                            Concert
-                        </p>
-                    </div>
-                    <div className="w-[331px] h-[37px] absolute left-0 top-0 overflow-hidden border-t-0 border-r-0 border-b border-l-0 border-white">
-                        <p className="w-[115.35px] h-[37px] absolute left-0 top-0 text-[15px] font-bold text-center text-white">
-                            행사명
-                        </p>
-                        <p className="w-[215.65px] h-[37px] absolute left-[115.35px] top-0 text-[15px] font-bold text-center text-white">
-                            KB IT’s Your Life
-                        </p>
-                    </div>
+            <div className="w-[331px] h-[580px] absolute left-[22px] top-[125px] overflow-hidden">
+                <div className="w-[330px] h-[148px] absolute left-0 top-0 overflow-hidden">
+                    <List title="행사명" content={ticket.eventTitle}/>
+                    <List title="행사 분류" content={ticket.eventType}/>
+                    <List title="행사 기간" content={`${ticket.startDate} ~ ${ticket.endDate}`}/>
+                    <List title="행사 장소" content={ticket.place}/>
                 </div>
                 <div className="w-[330px] h-[445px] absolute left-0 top-[148px] overflow-hidden">
                     <div className="w-[330px] h-[244px] absolute left-0 top-[-1px] overflow-hidden">
-                        <p className="w-[330px] absolute left-0 top-[60px] font-bold text-center text-white">
-                            <span className="w-[330px] text-xl font-bold text-center text-white">
-                                해당 행사를 삭제하시겠습니까?
-                            </span>
-                            <br />
-                            <span className="w-[330px] text-[15px] font-bold text-center text-white">
-                                삭제된 행사는 복구할 수 없으며,
-                            </span>
-                            <br />
-                            <span className="w-[330px] text-[15px] font-bold text-center text-white">
-                                기존에 등록된 리뷰 및 이미지가 모두 삭제됩니다.
-                            </span>
+                        <p className="w-[330px] absolute left-0 top-[60px] font-bold text-center text-[16px] text-white">
+                            <p>해당 행사를 삭제하시겠습니까?</p>
+                            <p>삭제된 행사는 복구할 수 없으며,</p>
+                            <p>기존에 등록된 리뷰 및 이미지가 모두 삭제됩니다.</p>
                         </p>
                     </div>
-                    <div className="w-[337px] h-[202px] absolute left-0 top-[243px] overflow-hidden">
-                        <div className="w-20 h-10 absolute left-[204px] top-[81px] overflow-hidden rounded-[15px] bg-[#ff9f0e]">
-                            <p className="w-20 h-10 absolute left-0 top-0 text-[15px] font-bold text-center text-black">
-                                return
-                            </p>
-                        </div>
-                        <div className="w-20 h-10 absolute left-[53px] top-[81px]">
-                            <div className="w-20 h-10 absolute left-[-1px] top-[-1px] rounded-[15px] bg-[#fd3030]" />
-                            <p className="w-20 h-10 absolute left-0 top-0 text-[15px] font-bold text-center text-black">
-                                Delete
-                            </p>
-                        </div>
+                    <div className="w-[337px] h-[202px] absolute left-0 top-[243px] overflow-hidden flex justify-between items-center px-10">
+                        <Button title="Delete" type="delete" func={()=>{}} />
+                        <Button title="return" type="default" func={()=>{}} />
                     </div>
                 </div>
             </div>
-            <div className="w-[375px] h-[53px] absolute left-0 top-[759px] overflow-hidden bg-[#454545]">
-                <div className="w-[125px] h-[53px] absolute left-[249px] top-[-1px] rounded-tl-[20px] bg-[#d9d9d9]" />
-                <div className="w-[125px] h-[53px] absolute left-[124px] top-[-1px] bg-[#454545]" />
-                <div className="w-[125px] h-[53px] absolute left-[-1px] top-[-1px] rounded-tr-[20px] bg-[#d9d9d9]" />
-                <div className="w-[23px] h-[39.26px]">
-                    <div className="absolute left-[198.5px] top-[5.5px]" />
-                    <img
-                        className="w-[23.82px] h-[39px] absolute left-[198.5px] top-[5.5px]"
-                        src="small_logo.png"
-                    />
-                </div>
-            </div>
-            <div className="w-[331px] h-[125px] absolute left-[22px] top-0">
-                <div className="w-[331px] h-[125px] absolute left-0 top-0 overflow-hidden">
-                    <div className="w-[331px] h-[30px] absolute left-0 top-0 overflow-hidden" />
-                    <div className="w-[331px] h-[95px] absolute left-0 top-[30px] overflow-hidden">
-                        <svg
-                            width={74}
-                            height={95}
-                            viewBox="0 0 74 95"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[74px] h-[95px] absolute left-[257px] top-0"
-                            preserveAspectRatio="none"
-                        >
-                            <circle cx={37} cy={48} r={3} fill="#FF6000" />
-                            <circle cx={28} cy={48} r={3} fill="#FF6000" />
-                            <circle cx={46} cy={48} r={3} fill="#FF6000" />
-                        </svg>
-                        <div className="w-[183px] h-[95px] absolute left-[74px] top-0 overflow-hidden">
-                            <p className="w-[183px] h-[95px] absolute left-0 top-0 text-[23px] text-center text-white">
-                                티켓 삭제
-                            </p>
-                        </div>
-                        <svg
-                            width={74}
-                            height={95}
-                            viewBox="0 0 74 95"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[74px] h-[95px] absolute left-0 top-0"
-                            preserveAspectRatio="none"
-                        >
-                            <path
-                                d="M35 55L28 48M28 48L35 41M28 48L46 48"
-                                stroke="white"
-                                strokeWidth={2}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
-                    </div>
-                </div>
-            </div>
+            <NavBar state="2"/>
+            <Header title="티켓 삭제" />
         </div>
     )
 }
--- src/pages/Member/index.tsx
+++ src/pages/Member/index.tsx
@@ -1,13 +1,9 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Login from "../Auth";
-import axios from "axios";
-import { login, postLogin } from "../../redux/slice/authSilce";
-import { AppDispatch } from "../../redux/store";
+import IndexHeader from "../../components/Header/IndexHeader";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Member = () => {
 	return(
-		<div className="w-[375px] h-[812px] relative overflow-hidden bg-white">
+		<div className="w-[375px] h-[812px] relative overflow-hidden bg-white flex justify-center">
 			<svg
 				width={375}
 				height={812}
@@ -19,29 +15,7 @@ const Member = () => {
 			>
 				<path d="M0 0H375V812H0V0Z" fill="#454545" />
 			</svg>
-			<div className="w-[331px] h-[125px] absolute left-[22px] top-0 overflow-hidden">
-				<div className="w-[331px] h-[30px] absolute left-0 top-0 overflow-hidden" />
-				<div className="w-[331px] h-[95px] absolute left-0 top-[30px] overflow-hidden">
-					<svg
-						width={74}
-						height={95}
-						viewBox="0 0 74 95"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						className="w-[74px] h-[95px] absolute left-[257px] top-0"
-						preserveAspectRatio="none"
-					>
-						<circle cx={37} cy={48} r={3} fill="#FF9F0E" />
-						<circle cx={28} cy={48} r={3} fill="#FF9F0E" />
-						<circle cx={46} cy={48} r={3} fill="#FF9F0E" />
-					</svg>
-					<div className="w-[257px] h-[95px] absolute left-0 top-0 overflow-hidden">
-						<p className="w-[257px] h-[95px] absolute left-0 top-0 text-[40px] font-medium text-left text-white">
-						User name
-						</p>
-					</div>
-				</div>
-			</div>
+			<IndexHeader title="User Name" />
 			<div className="w-[331px] h-[622px] absolute left-[22px] top-[125px]" />
 				<div className="w-[153px] h-[156px] absolute left-[199px] top-[535px] overflow-hidden">
 					<div className="w-[152px] h-[156px] absolute left-0 top-[-1px] bg-[#d9d9d9]" />
@@ -101,19 +75,8 @@ const Member = () => {
 				전체 이벤트
 				</p>
 			</div>
-			<div className="w-[375px] h-[53px] absolute left-0 top-[759px] overflow-hidden bg-[#454545]">
-				<div className="w-[125px] h-[53px] absolute left-[249px] top-[-1px] rounded-tl-[20px] bg-[#d9d9d9]" />
-				<div className="w-[125px] h-[53px] absolute left-[124px] top-[-1px] bg-[#454545]" />
-				<div className="w-[125px] h-[53px] absolute left-[-1px] top-[-1px] rounded-tr-[20px] bg-[#d9d9d9]" />
-				<div className="w-[23px] h-[39.26px]">
-				<div className="absolute left-[198.5px] top-[5.5px]" />
-				<img
-					className="w-[23.82px] h-[39px] absolute left-[198.5px] top-[5.5px]"
-					src="small_logo.png"
-				/>
-				</div>
-			</div>
-			</div>
+			<NavBar state="2"/>
+		</div>
 	)
 }
 
