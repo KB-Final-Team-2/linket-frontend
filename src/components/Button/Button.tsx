703f373d629008a08d0bbdc3d3cbb14a3fb31683
UBiiiii <willowman55@gmail.com>
2023-05-16 15:25:39 +0900

    [Feat] Button type 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,10 +7,10 @@ interface props {
 const Button = (props: props) => {
     return (
         <div
-            className="
-                w-20 h-10 left-[148px] top-5 overflow-hidden rounded-[15px] bg-[#ff9f0e]
+            className={`
+                w-20 h-10 left-[148px] top-5 overflow-hidden rounded-[15px] ${props.type=="delete" ? "bg-[#ff9f0e]" : "bg-red-600"}
                 text-[15px] font-bold text-black flex justify-center items-center
-            "
+            `}
             onClick={()=>{props.func()}}
         >
             {props.title}
