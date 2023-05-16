6148eb9e923a76c2bb1c12bac6ba7dc8a40f6525
skybluelion <brotherjun96@gmail.com>
2023-05-16 23:58:51 +0900

    [Feat] #10 Staff 사용자 정보 관련 페이지 navbar 컴포넌트

--- src/pages/Staff/InfoUserInfo.tsx
+++ src/pages/Staff/InfoUserInfo.tsx
@@ -1,5 +1,6 @@
 import Header from "../../components/Header/Header";
 import LargeList from "../../components/List/LargeList";
+import NavBar from "../../components/NavBar/NavBar";
 
 const InfoUserInfo = () => {
     const list = [{ title: "이름", content: "김고객" }, { title: "이메일", content: "qwer123@gmail.com" }, { title: "생일", content: "1999.01.01" }, { title: "성별", content: "남자" }, { title: "연락처", content: "010-1234-4567" }]
@@ -23,19 +24,7 @@ const InfoUserInfo = () => {
                     return (<LargeList title={el.title} content={el.content} />)
                 })}
             </div>
-            <div className="w-[375px] h-[53px] absolute left-0 top-[759px] overflow-hidden bg-[#454545]">
-                <div className="w-[125px] h-[53px] absolute left-[249px] top-[-1px] bg-[#454545]" />
-                <div className="w-[125px] h-[53px] absolute left-[124px] top-[-1px] rounded-tr-[20px] bg-[#d9d9d9]" />
-                <div className="w-[125px] h-[53px] absolute left-[-1px] top-[-1px] bg-[#d9d9d9]" />
-                <div className="w-6 h-6 absolute left-[301px] top-3" />
-                <div className="w-[23px] h-[39.26px]">
-                    <div className="absolute left-[198.5px] top-[5.5px]" />
-                    <img
-                        className="w-[23.82px] h-[39px] absolute left-[198.5px] top-[5.5px]"
-                        src="small_logo.png"
-                    />
-                </div>
-            </div>
+			<NavBar state="3"/>
         </div>
     )
 }
