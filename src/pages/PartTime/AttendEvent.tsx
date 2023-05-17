cc81fd18ae4ff9f5f9924495cfa7708ee5d6d0f8
UBiiiii <willowman55@gmail.com>
2023-05-17 13:28:52 +0900

    [Feat] 파트타임 직원용 페이지 구조 수정

--- src/pages/PartTime/AttendEvent.tsx
+++ src/pages/PartTime/AttendEvent.tsx
@@ -9,31 +9,20 @@ const AttendEvent = () => {
     { title: "안녕하세요", content: "반갑습니다." },
     { title: "안녕하세요", content: "반갑습니다." }];
     return (
-        <div className="w-[375px] h-[812px] relative overflow-hidden bg-white">
-            <svg
-                width={375}
-                height={812}
-                viewBox="0 0 375 812"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-[375px] h-[812px] absolute left-0 top-0"
-                preserveAspectRatio="none"
-            >
-                <path d="M0 0H375V812H0V0Z" fill="#454545" />
-            </svg>
-
-            <div className="w-[331px] h-[622px] absolute left-[22px] top-[125px]" />
-
-            <div className="w-[331px] h-[622px] absolute left-[22px] top-[125px] overflow-hidden">
-                <div className="w-[330px] h-[580px] absolute left-0 top-0 overflow-hidden border border-white">
-                    <TableInfo title="행사명" content="KB IT’s Your Life" />
-                    {list.map((el) => {
-                        return (<NextList title={el.content} />)
-                    })}
+        <div className="w-[375px] h-[812px] relative overflow-hidden bg-background-dark flex flex-col justify-center items-center">
+            <Header title="근태 관리" />
+            <div className="w-[331px] h-full flex flex-col">
+                <div className="w-[331px] h-[622px] overflow-hidden">
+                    <div className="w-[330px] h-[580px] overflow-hidden border-y border-white">
+                        <TableInfo title="행사명" content="KB IT’s Your Life" />
+                        {list.map((el) => {
+                            return (<NextList title={el.content} />)
+                        })}
+                    </div>
                 </div>
             </div>
             <NavBar state="1" />
-            <Header title="근태 관리" />
+
         </div>
     )
 }
