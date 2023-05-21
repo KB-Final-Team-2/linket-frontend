1f8e1a1f7be01324a2e6fa2c05a2518363300e76
UBiiiii <willowman55@gmail.com>
2023-05-21 21:02:43 +0900

    [Refactor] userSlice 삭제

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './slice/userSilce'
 import authReducer from './slice/authSilce'
 import eventReducer from './slice/eventSlice'
 import ticketReducer from './slice/ticketSlice'
@@ -11,7 +10,6 @@ import modalReducer from './slice/modalSlice'
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    user: userReducer,
     event: eventReducer,
     ticket: ticketReducer,
     review: reviewReducer,
