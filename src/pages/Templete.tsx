2a22983f24b7bc6a6f422e1cea014a263913804f
UBiiiii <willowman55@gmail.com>
2023-05-22 16:04:30 +0900

    [Feat] 비로그인시 로그인 페이지로 이동하도록 기능 추가

--- src/pages/Templete.tsx
+++ src/pages/Templete.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Templete = ({children} : any) => {
+    const user = useSelector((state:any)=>state.auth.data);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(user.role==="")  navigate("/login")
+    }, [])
+
     return (
         <div className="w-full h-full relative overflow-hidden templete flex flex-col justify-center items-center">
             {children}
