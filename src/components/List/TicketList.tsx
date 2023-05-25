81c13d03c60ecb6648d0ba08a41655c47c3d8d7d
UBiiiii <willowman55@gmail.com>
2023-05-25 13:36:55 +0900

    [Feat] Ticket 디자인 변경 및 데이터 구성 변경

--- src/components/List/TicketList.tsx
+++ src/components/List/TicketList.tsx
@@ -1,14 +1,37 @@
 import { FuncListProps } from "../../interface/props";
+import TicketBackground from "../../img/ticket2.png"
+import Barcode from "react-barcode";
+import { TicketWithEvent } from "../../interface/Ticket";
+import { useDispatch } from "react-redux";
+import { getTicket, setTicket } from "../../redux/slice/ticketSlice";
 
-const TicketList = (props : FuncListProps) => {
-    return(
+interface props {
+    ticket: TicketWithEvent;
+}
+
+const TicketList = ({ ticket }: props) => {
+    const dispatch: any = useDispatch();
+
+    return (
         <div
-            className="w-[330px] h-20 overflow-hidden border-[5px] border-[#ffa559]"
-            onClick={()=>{props.func()}}    
+            className="w-full h-20 overflow-hidden cursor-pointer flex-shrink-0 flex justify-start items-center"
+            style={{
+                background: `url(${TicketBackground})`
+            }}
+            // onClick={() => { dispatch(getTicket(ticket.ticketId)) }}
+            onClick={() => { dispatch(setTicket(ticket)) }}
         >
-            <p className="text-[40px] font-bold text-center text-[#ffa559]">
-                {props.title}
-            </p>
+            <div className="w-[60px] h-[30px] rotate-90 overflow-hidden flex place-content-center flex-shrink-0">
+                <Barcode value={ticket.serialNum} width={1} height={40} background="none" displayValue={false} margin={0} />
+            </div>
+            <div className="w-full text-[30px] flex flex-col flex-grow-0 px-4">
+                <div className="w-full h-full font-bold text-[#ffa559] truncate flex justify-start items-center">
+                    {ticket.eventName}
+                </div>
+                <div className="w-full h-full font-bold text-base text-[#d9d9d9] flex justify-start items-center">
+                    {ticket.startDate}~{ticket.endDate}
+                </div>
+            </div>
         </div>
     )
 };
--- src/interface/Ticket.ts
+++ src/interface/Ticket.ts
@@ -11,6 +11,35 @@ export interface Ticket {
     ticketStatus: string;
     regDate: string;
     lastUpdatedDate: string;
+    seat: string;
+}
+
+export interface TicketWithEvent extends Ticket, Event {};
+
+export interface TicketWithUser extends Ticket, User {};
+
+export const DUMMY_TICKETWITHEVENT : TicketWithEvent = {
+    ticketId: 1,
+    eventId: 1,
+    companyId: 1,
+    email: "member@test.com",
+    serialNum: "123456",
+    ticketReg: "Y",
+    ticketStatus: "Y",
+    regDate: "2023-03-01",
+    lastUpdatedDate: "",
+    eventName: "KB Killing Boys Concert",
+    eventType: "concert",
+    place: "잠실 올림픽 경기장",
+    eventImage: "event1.jpg",
+    eventInq: "02-123-456",
+    eventDesc: "대세 남자 아이돌 콘서트",
+    eventStatus: "Y",
+    startDate: "2023.05.17",
+    endDate: "2023.05.17",
+    deletedDate: "",
+    seat: "s",
+    link: ""
 }
 
 export const INIT_TICKET : Ticket = {
@@ -22,7 +51,8 @@ export const INIT_TICKET : Ticket = {
     ticketReg: "",
     ticketStatus: "",
     regDate: "",
-    lastUpdatedDate: ""
+    lastUpdatedDate: "",
+    seat: "s"
 }
 
 export const DUMMY_TICKET1 : Ticket = {
@@ -34,7 +64,8 @@ export const DUMMY_TICKET1 : Ticket = {
     ticketReg: "Y",
     ticketStatus: "Y",
     regDate: "2023-03-01",
-    lastUpdatedDate: ""
+    lastUpdatedDate: "",
+    seat: "s"
 }
 
 export const DUMMY_TICKET2 : Ticket = {
@@ -46,5 +77,29 @@ export const DUMMY_TICKET2 : Ticket = {
     ticketReg: "Y",
     ticketStatus: "Y",
     regDate: "2023-03-01",
-    lastUpdatedDate: ""
+    lastUpdatedDate: "",
+    seat: "s"
+}
+
+export const DUMMY_TICKETWITHUSER : TicketWithUser = {
+    ticketId: 1,
+    eventId: 1,
+    companyId: 1,
+    email: "member@test.com",
+    serialNum: "123456",
+    ticketReg: "Y",
+    ticketStatus: "Y",
+    regDate: "2023-03-01",
+    lastUpdatedDate: "",
+    password: "1234",
+    userName: "Member",
+    birthDate: "2023-05-18",
+    phone: "010-1234-1234",
+    gender: "M",
+    role: "member",
+    aggrement: false,
+    account: "",
+    withdrawDate: "",
+    enable: "",
+    seat: "s"
 }
