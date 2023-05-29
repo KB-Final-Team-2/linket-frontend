9c95e8566e534e53b8196fbdeacd00e9f7a11378
UBiiiii <willowman55@gmail.com>
2023-05-29 13:04:54 +0900

    [Feat] #45 firebase 모듈 설치 및 기본 설정

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnRG1QPQUYgD9DYpqtHy7J_sLknevS29A",
+  authDomain: "linket-chat.firebaseapp.com",
+  projectId: "linket-chat",
+  storageBucket: "linket-chat.appspot.com",
+  messagingSenderId: "556079828689",
+  appId: "1:556079828689:web:8600ba7786b686ec8f7ff0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
